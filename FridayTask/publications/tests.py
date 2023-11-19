b08cd58ff2a3d9b737a30b504c28718d7c7d7831
Ramy <vladramis@mail.ru>
2023-11-19 16:42:02 +0300

    add test crud for create publications by form and test delete publications

--- FridayTask/publications/tests.py
+++ FridayTask/publications/tests.py
@@ -2,7 +2,8 @@ import pytest as pytest
 from django.test import TestCase
 from publications.models import Publications
 from mixer.backend.django import mixer
-
+from django.urls import reverse
+from .forms import PublicationsForm
 
 class TestPublications(TestCase):
 
@@ -19,4 +20,32 @@ class TestPublications(TestCase):
         )
         assert Publications.objects.count() == 1
         assert publication.title == "вмвмвм"
+
+    def test_publication_create_by_form(self):
+        data = {
+            'title': "Влад",
+            'full_text': 'Владислав',
+            'anons': "Влад",
+            'date': '2003-02-22'
+        }
+
+        form = PublicationsForm(data)
+        assert form.is_valid() == True
+        form.save()
+
+        assert Publications.objects.count() == 1
+
+    def test_delete_publication(self):
+        publication = mixer.blend(
+            Publications,
+            title="вмвмвм",
+            full_text="мввмымфыфсы",
+            html="<b>html here...</b>",
+        )
+
+        assert Publications.objects.count() == 1
+
+        self.client.post(reverse('publ-delete', kwargs={'pk': publication.id}))
+
+        assert Publications.objects.count() == 0
 
