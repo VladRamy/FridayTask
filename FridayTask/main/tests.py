0a6c02d91b431ba2c9b13bd0a8085555e2d38768
Ramy <vladramis@mail.ru>
2023-11-19 15:33:55 +0300

    test fixed

--- FridayTask/main/tests.py
+++ FridayTask/main/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class TestMainPage(TestCase):
 
     def test_main_paige_loads_without_errors(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('main:home'))
         html = response.content.decode("utf8")
         self.assertTrue(html.strip().startswith("<!doctype html>"))
         self.assertTrue(html.strip().endswith("</html>"))
