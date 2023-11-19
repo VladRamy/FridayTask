c7013771cc53c8384bc5520646eb83acde4bd0be
Ramy <vladramis@mail.ru>
2023-11-19 15:17:55 +0300

    test

--- FridayTask/main/tests.py
+++ FridayTask/main/tests.py
@@ -4,5 +4,8 @@ from django.urls import reverse
 class TestMainPage(TestCase):
 
     def test_main_paige_loads_without_errors(self):
-        response = self.client(reverse("main:index"))
+        response = self.client.get('/')
         html = response.content.decode("utf8")
+        self.assertTrue(html.strip().startswith("<!doctype html>"))
+        self.assertTrue(html.strip().endswith("</html>"))
+        self.assertTemplateUsed(response, "main/index.html")
